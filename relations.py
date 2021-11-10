ad20b2592d382cea1ecbdda5ff64659933315072
egprivalov <44251572+egprivalov@users.noreply.github.com>
2021-11-10 11:40:41 +0500

    Add files via upload
    Программа высчитывает весовые коэффициенты, по введенной матрице взаимоотношений критериев. Код закомментирован, ошибки ввода предусмотрены.

---
+++ relations.py
@@ -0,0 +1,62 @@
+from math import pow
+
+
+def tofixed(numobj, digits=2):  # Функция, возвращающая дробное число с двумя знаками после запятой(по умолчанию)
+    return float(f"{numobj:.{digits}f}")
+
+
+print("Программа расчитывает весовых коэффициентов")
+# Ввод количества критериев
+while True:  # Проверка ввода
+    try:
+        n = int(input("Введите количество критериев: "))
+        if n < 0:  # Проверка на положительность количества критериев
+            print("Количество критериев должно быть положительным числом")
+        else:
+            break
+    except ValueError:
+        print("Некорректный ввод!")
+        pass
+
+# Матрица взаимосвязей
+RelationMatrix = [[0.0] * n for _ in range(n)]
+
+# Ввод взаимосвязи критериев и заполнение матрицы взаимосвязей
+for i in range(n):
+    for j in range(i, n):
+        if i == j:
+            RelationMatrix[i][j] = 1.0
+        else:
+            while True:  # Проверка ввода
+                try:
+                    ijRelation = float(input(f"Введите отношение критерия {i+1} к критерию {j+1}: "))
+                    if n <= 0:  # Проверка на положительность отношения критериев
+                        print("Отношение критериев должно быть положительным числом, не равным нулю")
+                    else:
+                        break
+                except ValueError:
+                    print("Некорректный ввод!")
+                    pass
+            RelationMatrix[i][j] = tofixed(ijRelation)
+
+# Заполнение обратных взаимосвязей
+for i in range(n):
+    for j in range(i):
+        RelationMatrix[i][j] = tofixed(pow(RelationMatrix[j][i], -1))
+
+# Суммы матрицы
+sums = []  # Суммы каждой строки матрицы
+for i in range(n):
+    sums.append(sum(RelationMatrix[i]))
+# Сумма всех строк матрицы
+sumRM = sum(sums)
+
+# Массив весовых коэффициентов
+Weight = [tofixed(sums[i]/sumRM) for i in range(n)]
+
+# Поправка на погрешность
+Weight[Weight.index(max(Weight))] -= 1-sum(Weight)
+
+print()
+for i in range(n):
+    print(f"Весовой коэффициент критерия {i+1} равен: {Weight[i]}")
